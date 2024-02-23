cb419ca27d265233fd0cbd7fd59f1966991f3280
SedanLee <water8048@naver.com>
2024-02-23 16:31:26 +0900

    feat: serveyform 추가

--- src/components/ServeyForm/index.tsx
+++ src/components/ServeyForm/index.tsx
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
-const ServeyForm = () => {
-  const clickNext = () => {};
+
+const ServeyForm: React.FC = () => {
   const questionList = ["질문할게여", "비디오가 있나여?", "라이브웨딩이 있나여?", "이제 청첩장에 적을 내용 적어주세여"];
+  const answerList = ["있음", "없음"];
+
+  const [answers, setAnswers] = useState<Array<string | null>>(Array.from({ length: questionList.length }, () => null));
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+
+  const handleAnswerChange = (index: number, value: string) => {
+    const newAnswers = [...answers];
+    newAnswers[index] = value;
+    setAnswers(newAnswers);
+  };
+
+  const clickNext = () => {
+    if (
+      answers[currentQuestionIndex] !== null ||
+      currentQuestionIndex === 0 ||
+      currentQuestionIndex === questionList.length - 1
+    ) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      alert("답변을 선택해주세요.");
+    }
+  };
+
+  useEffect(() => {
+    if (currentQuestionIndex === 0) {
+      const timer = setTimeout(() => setCurrentQuestionIndex(1), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [currentQuestionIndex]);
   return (
     <S.ServeyFormContainer>
       <div>
         <ul>
-          {questionList.map((question, index) => (
-            <S.ServeyFormQuestionLi key={index}>
-              <h1>{question}</h1>
-            </S.ServeyFormQuestionLi>
+          {questionList.slice(0, currentQuestionIndex + 1).map((question, index) => (
+            <>
+              <S.ServeyFormQuestionLi>
+                <h1>{question}</h1>
+              </S.ServeyFormQuestionLi>
+              {index !== 0 && index !== questionList.length - 1 ? (
+                <S.ServeyFormQuestionLi key={index}>
+                  <>
+                    <div>
+                      <input
+                        type="radio"
+                        value="1"
+                        checked={answers[index] === "1"}
+                        onChange={() => handleAnswerChange(index, "1")}
+                      />
+                      <label>{answerList[0]}</label>
+                    </div>
+                    <div>
+                      <input
+                        type="radio"
+                        value="2"
+                        checked={answers[index] === "2"}
+                        onChange={() => handleAnswerChange(index, "2")}
+                      />
+                      <label>{answerList[1]}</label>
+                    </div>
+                    <button onClick={clickNext}>다음</button>
+                  </>
+                </S.ServeyFormQuestionLi>
+              ) : null}
+            </>
           ))}
         </ul>
       </div>
--- src/components/ServeyForm/style.ts
+++ src/components/ServeyForm/style.ts
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 
 export const ServeyFormContainer = styled.div``;
-export const ServeyFormChatUl = styled.ul``;
-export const ServeyFormQuestionLi = styled.li``;
+export const ServeyFormChatUl = styled.ul`
+  width: 100%;
+  height: 100%;
+`;
+export const ServeyFormQuestionLi = styled.li`
+  width: 150px;
+
+  background-color: gray;
+  margin: 30px 0;
+  border-radius: 4px 16px 16px 16px;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  padding: 15px 10px;
+  button {
+    border-radius: 8px;
+    border: none;
+  }
+`;
 export const ServeyFormAnswerLi = styled.li``;
