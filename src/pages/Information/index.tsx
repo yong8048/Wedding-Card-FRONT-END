b07f705841988f5128039d174fd89ddfc6b468d0
LeeByoungWook <lbw3973@naver.com>
2024-03-05 13:19:23 +0900

    feat: 웨딩 일정 및 장소 등록 컴포넌트 생성

--- src/pages/Information/index.tsx
+++ src/pages/Information/index.tsx
@@ -1,9 +1,12 @@
-import MainPhoto from "@/components/Information/MainPhoto";
 import * as S from "./style";
+import MainPhoto from "@/components/Information/MainPhoto";
 import HumanInfo from "@/components/Information/HumanInfo";
 import Greeting from "@/components/Information/Greeting";
 import ProgressBar from "@/components/Information/ProgressBar";
 import Contact from "@/components/Information/Contact";
+import Account from "@/components/Information/Account";
+import WeddingSchedule from "@/components/Information/WeddingSchedule";
+
 const Information = () => {
   return (
     <S.Section>
@@ -12,6 +15,8 @@ const Information = () => {
       <Greeting />
       <HumanInfo />
       <Contact />
+      <Account />
+      <WeddingSchedule />
     </S.Section>
   );
 };
