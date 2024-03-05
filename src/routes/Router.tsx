49cc03033e887ad9a310a7781f198de9ba64de5f
SedanLee <water8048@naver.com>
2024-03-05 13:23:50 +0900

    feat: select페이지 라우터 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
 import Servey from "@/pages/Servey";
 import Auth from "@/pages/Auth";
+import Select from "@/pages/Select";
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/servey" element={<Servey />} />
+          <Route path="/select" element={<Select />} />
           <Route path="/auth" element={<Auth />} />
         </Route>
       </Routes>
