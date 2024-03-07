50f5947f7ea35c2cf67a196b73fd958a14ba6169
LeeByoungWook <lbw3973@naver.com>
2024-03-08 02:27:08 +0900

    feat: 전화번호 작성 데이터 담기 구현

--- src/components/Information/Contact/index.tsx
+++ src/components/Information/Contact/index.tsx
@@ -1,16 +1,41 @@
+import { IReqCreateInvitation, TconcernedParentType, TconcernedPersonType } from "@/types/invitation";
 import * as S from "./style";
+import React from "react";
 
-const Contact = () => {
+const Contact = ({
+  setCreateInvitaionData,
+}: {
+  setCreateInvitaionData: React.Dispatch<React.SetStateAction<IReqCreateInvitation>>;
+}) => {
+  const handleDataChange = (e: React.ChangeEvent<HTMLDivElement>) => {
+    const divEl = e.currentTarget;
+    const inputEl = e.target as HTMLInputElement;
+    const concernedPerson = divEl.id as TconcernedPersonType;
+
+    const { name, value } = inputEl;
+    const concernedParent = name as TconcernedParentType;
+
+    setCreateInvitaionData(previousData => ({
+      ...previousData,
+      [concernedPerson]: {
+        ...previousData[concernedPerson],
+        [concernedParent]: {
+          ...previousData[concernedPerson][concernedParent],
+          contact: value,
+        },
+      },
+    }));
+  };
   return (
     <S.Container>
       {["신랑", "신부"].map((value, index) => (
         <S.Wrapper key={index} $isFirst={value === "신랑"}>
           <h1>{value} 및 혼주의 연락받을 연락처를 작성해주세요.</h1>
           <h3>' - '를 제외하고 입력해주세요.</h3>
-          <div>
-            <input placeholder={`${value} 전화번호`} />
-            <input placeholder={`${value} 부 전화번호`} />
-            <input placeholder={`${value} 모 전화번호`} />
+          <div onChange={handleDataChange} id={value === "신랑" ? "husband" : "wife"}>
+            <input placeholder={`${value} 전화번호`} name="me" type="number" />
+            <input placeholder={`${value} 부 전화번호`} name="father" type="number" />
+            <input placeholder={`${value} 모 전화번호`} name="mother" type="number" />
           </div>
         </S.Wrapper>
       ))}
