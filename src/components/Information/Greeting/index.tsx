5e272eed1a40871cd0f54f848c7f45a9c40fed5a
LeeByoungWook <lbw3973@naver.com>
2024-03-08 02:26:41 +0900

    feat: 인사말 작성 데이터 담기 구현

--- src/components/Information/Greeting/index.tsx
+++ src/components/Information/Greeting/index.tsx
@@ -1,13 +1,22 @@
+import { IReqCreateInvitation } from "@/types/invitation";
 import * as S from "./style";
 
-const Greeting = () => {
+const Greeting = ({
+  setCreateInvitaionData,
+}: {
+  setCreateInvitaionData: React.Dispatch<React.SetStateAction<IReqCreateInvitation>>;
+}) => {
+  const handleDataChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCreateInvitaionData(previousData => ({ ...previousData, welcome: e.target.value }));
+  };
+
   return (
     <S.Container>
       <h1>인사말을 작성해 주세요.</h1>
       <h3>작성한 내용을 토대로 제작됩니다.</h3>
       <h3>띄어쓰기나 줄바꿈에 유의해주세요.</h3>
       <div className="textarea">
-        <textarea placeholder="인사말 작성" />
+        <textarea placeholder="인사말 작성" onChange={handleDataChange} />
       </div>
     </S.Container>
   );
