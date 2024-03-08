8d6e82dc78ece6f586577abec044bfa0a8317b64
SedanLee <water8048@naver.com>
2024-03-09 03:40:40 +0900

    백업

---
+++ src/components/SelectModal/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from "./style";
+const SelectModal = () => {
+  return (
+    <S.SelectModalContainer>
+      <span>페이지</span>
+    </S.SelectModalContainer>
+  );
+};
+
+export default SelectModal;
