06bedf0d44c87996541bbaee5749bc20e75b461e
LeeByoungWook <lbw3973@naver.com>
2024-03-14 22:52:42 +0900

    test: cors 테스트

--- src/components/Information/test/index.tsx
+++ src/components/Information/test/index.tsx
@@ -1,8 +1,8 @@
 // import MDEditor from "@uiw/react-md-editor";
 import * as S from "./style";
 // import { useState } from "react";
-import { Editor } from "@toast-ui/react-editor";
-import "@toast-ui/editor/dist/toastui-editor.css";
+// import { Editor } from "@toast-ui/react-editor";
+// import "@toast-ui/editor/dist/toastui-editor.css";
 
 const Test = () => {
   // const [value, setValue] = useState<string | undefined>("");
@@ -10,7 +10,7 @@ const Test = () => {
   return (
     <S.Container>
       {/* <MDEditor height={300} value={value} onChange={setValue} /> */}
-      <Editor initialValue="" />
+      {/* <Editor initialValue="" /> */}
     </S.Container>
   );
 };
