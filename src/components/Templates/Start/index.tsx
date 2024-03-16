13551d5fab1ac0a79753c9965a26a5040f14290d
yong8048 <water8048@naver.com>
2024-03-16 17:45:28 +0900

    feat: templates 예시 추가

---
+++ src/components/Templates/Start/index.tsx
@@ -0,0 +1,92 @@
+import * as S from "./style";
+const Start = () => {
+  return (
+    <S.TemplateContainer>
+      <S.Page1Div>
+        <S.Page1Date>
+          <p>02月24日</p>
+        </S.Page1Date>
+        <img src="/img1.jpg" alt="사진" />
+        <S.Page1Info>
+          <h1>조명철 / 이병욱</h1>
+          <h2>토요일 오후 1시 30분</h2>
+          <h2>수원 웨딩홀</h2>
+        </S.Page1Info>
+      </S.Page1Div>
+      <div>
+        <div>
+          <div></div>
+          <h1>이제 서로, 평생 함께</h1>
+          <h2>서로가 마주보며 다져온 사랑을</h2>
+          <h2>이제 함께 한 곳을 바라보며</h2>
+          <h2>걸어갈 수 있는 큰 사랑으로</h2>
+          <h2>키우고자 합니다.</h2>
+          <h2>저희 두 사람이 사랑의 이름으로</h2>
+          <h2>지켜나갈 수 있도록</h2>
+          <h2>앞날을 축복해 주시면</h2>
+          <h2>감사하겠습니다.</h2>
+          <h3>이석훈 · 이미자의 장남 조명철</h3>
+          <h3>유성령 · 박효순의 장남 이병욱</h3>
+        </div>
+        <div>
+          <h1>소중한 당신을 초대합니다.</h1>
+        </div>
+        <div>
+          <div>
+            <h1>신랑에게 연락하기</h1>
+            <button>전화</button>
+            <button>메세지</button>
+          </div>
+          <div>
+            <h1>신부에게 연락하기</h1>
+            <button>전화</button>
+            <button>메세지</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1>혼주에게 연락하기</h1>
+          </div>
+          <div>
+            <div>
+              <h1>신랑 측 혼주</h1>
+              <div>
+                <h1>아버지 이석훈</h1>
+                <div>
+                  <button>전화</button>
+                  <button>메세지</button>
+                </div>
+              </div>
+              <div>
+                <h1>어머니 이미자</h1>
+                <div>
+                  <button>전화</button>
+                  <button>메세지</button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h1>신부 측 혼주</h1>
+              <div>
+                <h1>아버지 유성령</h1>
+                <div>
+                  <button>전화</button>
+                  <button>메세지</button>
+                </div>
+              </div>
+              <div>
+                <h1>어머니 박효순</h1>
+                <div>
+                  <button>전화</button>
+                  <button>메세지</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </S.TemplateContainer>
+  );
+};
+
+export default Start;
