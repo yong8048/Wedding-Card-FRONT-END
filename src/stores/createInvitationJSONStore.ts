e92564408faa750d1c056095f23586974603953d
LeeByoungWook <lbw3973@naver.com>
2024-03-16 16:33:26 +0900

    fix: 주석 제거

--- src/stores/createInvitationJSONStore.ts
+++ src/stores/createInvitationJSONStore.ts
@@ -3,6 +3,6 @@ import { IReqInvitationJSON } from "@/types/invitation";
 import { InitialData_CreateInvitationJSON } from "@/utils/InitialData";
 
 export const invitationJSONState = atom<IReqInvitationJSON>({
-  key: "invitationJSONState", // 고유한 키
-  default: InitialData_CreateInvitationJSON, // 초기 상태 값
+  key: "invitationJSONState",
+  default: InitialData_CreateInvitationJSON,
 });
--- src/stores/createInvitationPhotosStore.ts
+++ src/stores/createInvitationPhotosStore.ts
@@ -3,6 +3,6 @@ import { IReqInvitationPhotos } from "@/types/invitation";
 import { InitialData_CreateInvitationPhotos } from "@/utils/InitialData";
 
 export const invitationPhotosState = atom<IReqInvitationPhotos>({
-  key: "invitationPhotosState", // 고유한 키
-  default: InitialData_CreateInvitationPhotos, // 초기 상태 값
+  key: "invitationPhotosState",
+  default: InitialData_CreateInvitationPhotos,
 });
