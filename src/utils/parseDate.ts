11047b43c50b6bc1fbe9fed7b08380254bacb2e2
LeeByoungWook <lbw3973@naver.com>
2024-03-19 03:24:41 +0900

    feat: getTime 커스텀 훅 추가 구현

--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -28,3 +28,27 @@ export const getFullDate = (date: Date) => {
 
   return `${year}년 ${month}월 ${day}일 ${dayOfWeek}요일 ${hour > 12 ? `오후 ${hour % 12}` : `오전 ${hour}`}시 ${minute ? `${minute}분` : ``}`;
 };
+
+export const getDateWithDots = (date: Date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}.${month}.${day}`;
+};
+
+export const getDayWithTime = (date: Date) => {
+  const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"][date.getDay()];
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+
+  return `${dayOfWeek}요일 ${hour > 12 ? `오후 ${hour % 12}` : `오전 ${hour}`}시 ${minute ? `${minute}분` : ``}`;
+};
+
+export const getDday = (date: Date) => {
+  const now = new Date();
+  const dDayTime = Math.abs(date.getTime() - now.getTime());
+  const dDay = Math.ceil(dDayTime / (1000 * 60 * 60 * 24));
+
+  return dDay;
+};
