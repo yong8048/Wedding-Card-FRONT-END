772d5760fe0438d93dac8699029b90dcb47c8a3b
LeeByoungWook <lbw3973@naver.com>
2024-03-18 15:45:04 +0900

    design: main 태그 padding 및 margin 추가

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Main = styled.main`
   max-width: 768px;
   text-align: center;
-  margin-bottom: 70px;
+  margin-bottom: 60px;
+  padding-bottom: 30px;
 `;
